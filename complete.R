bd991fa7119a29cb5afc75ca7172f0b5eba3ccf8
sehahn <sehahn59@gmail.com>
2014-12-17 11:41:21 -0600

    Calculates number of complete observations per monitor
    Calculates number of complete observations per monitor using function
    call complete(directory, ID) where directory is the subdirectory (of the
    R working directory) containing the monitor data files, and the ID is
    the id number or range of numbers for the monitor(s) of interest. Has
    some odd bits to deal with decreasing ID ranges.

--- complete.R
+++ complete.R
@@ -1,3 +1,9 @@
+# Calculates number of complete observations per monitor using function
+# call complete(directory, ID) where directory is the subdirectory (of the
+# R working directory) containing the monitor data files, and the ID is the
+# id number or range of numbers for the monitor(s) of interest. Has some
+# odd bits to deal with decreasing ID ranges.
+
 complete <- function(directory, id=1:332) {
   allfiles <- list.files(directory, full.names=TRUE)
 #  allfiles
